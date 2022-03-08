a267d9c935e5c7eff57361b2684b02968c6c86f4
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-08 20:55:42 +0800

    feat: faucet contracts

---
+++ contracts/Faucet.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+  address[] public funders;
+
+  receive() external payable {}
+
+  function addFunds() external payable {
+    funders.push(msg.sender);
+  }
+}
