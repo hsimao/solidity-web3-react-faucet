670b825edeb6648e0b92e83de6298a8293920068
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-12 18:34:44 +0800

    feat: faucet contracts logic

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,11 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  address[] public funders;
+  uint256 public numOfFunders;
+  mapping(address => bool) private funders;
+  mapping(uint256 => address) private lutFunders;
 
   receive() external payable {}
 
   function addFunds() external payable {
-    funders.push(msg.sender);
+    address funder = msg.sender;
+
+    if (!funders[funder]) {
+      uint256 index = numOfFunders++;
+      funders[funder] = true;
+      lutFunders[index] = funder;
+    }
+  }
+
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+
+    for (uint256 i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+
+    return _funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view returns (address) {
+    return lutFunders[index];
   }
 }
