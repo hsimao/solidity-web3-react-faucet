da93088e65e38203addc96eb07a03024ecdf34e3
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-13 14:10:30 +0800

    feat: basic css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import "./App.css";
+
 function App() {
-  return <div className="App">Hellow</div>;
+  return (
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <string>10</string> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
